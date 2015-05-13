f697a01a8d611bad5aeb2cb524347238ea6ca1b2
Alexander Schieweck <alexander.schieweck@tu-dortmund.de>
2015-05-13 09:36:05 +0000

    refactored the generation of internal data structures.

--- main/src/main/java/de/learnlib/alex/algorithms/DiscriminationTree.java
+++ main/src/main/java/de/learnlib/alex/algorithms/DiscriminationTree.java
@@ -1,12 +1,14 @@
 package de.learnlib.alex.algorithms;
 
 import de.learnlib.alex.annotations.LearnAlgorithm;
-import de.learnlib.alex.utils.DiscriminationTreeSerializer;
+import de.learnlib.algorithms.discriminationtree.hypothesis.HState;
 import de.learnlib.algorithms.discriminationtree.mealy.DTLearnerMealy;
 import de.learnlib.algorithms.discriminationtree.mealy.DTLearnerMealyBuilder;
 import de.learnlib.api.LearningAlgorithm;
+import de.learnlib.discriminationtree.DTNode;
 import de.learnlib.oracles.SULOracle;
 import net.automatalib.words.Alphabet;
+import net.automatalib.words.Word;
 
 /**
  * Class that provides the LearnLib implementation of the Discrimination Tree algorithm for ALEX.
@@ -28,7 +30,56 @@ public class DiscriminationTree implements LearnAlgorithmFactory {
         }
         de.learnlib.discriminationtree.DiscriminationTree discriminationTree;
         discriminationTree = ((DTLearnerMealy) learner).getDiscriminationTree();
-        return DiscriminationTreeSerializer.toJSON(discriminationTree);
+        return toJSON(discriminationTree);
+    }
+
+    /**
+     * Serializes the discrimination tree of the discrimination tree algorithm into JSON.
+     *
+     * @param tree
+     *         The tree to convert into nice JSON.
+     * @return The JSON string of the given tree.
+     */
+    private String toJSON(de.learnlib.discriminationtree.DiscriminationTree<String, Word, HState> tree) {
+        return toJSON(tree.getRoot());
+    }
+
+    private String toJSON(DTNode<String, Word, HState> node) {
+        StringBuilder result = new StringBuilder();
+        result.append('{');
+
+        if (node.getParentOutcome() != null) {
+            result.append("\"edgeLabel\": \"");
+            result.append(node.getParentOutcome());
+            result.append("\",");
+        }
+
+        if (node.isLeaf()) {
+            result.append("\"data\": \"");
+            result.append(node.getData());
+            result.append('"');
+        } else {
+            result.append("\"discriminator\": \"");
+            result.append(node.getDiscriminator());
+            result.append("\", ");
+
+            result.append("\"children\": [");
+            node.getChildEntries().forEach(entry -> {
+                DTNode child = entry.getValue();
+                result.append(toJSON(child));
+                result.append(",");
+            });
+
+            // remove last ','
+            if (result.charAt(result.length() - 1) == ',') {
+                result.setLength(result.length() - 1);
+            }
+
+            result.append(']');
+        }
+
+        result.append('}');
+        return result.toString();
     }
 
 }
--- main/src/main/java/de/learnlib/alex/algorithms/TTT.java
+++ main/src/main/java/de/learnlib/alex/algorithms/TTT.java
@@ -1,12 +1,13 @@
 package de.learnlib.alex.algorithms;
 
 import de.learnlib.alex.annotations.LearnAlgorithm;
-import de.learnlib.alex.utils.TTTSerializer;
+import de.learnlib.algorithms.ttt.base.*;
 import de.learnlib.algorithms.ttt.mealy.TTTLearnerMealy;
 import de.learnlib.algorithms.ttt.mealy.TTTLearnerMealyBuilder;
 import de.learnlib.api.LearningAlgorithm;
 import de.learnlib.oracles.SULOracle;
 import net.automatalib.words.Alphabet;
+import net.automatalib.words.Word;
 
 /**
  * Class that provides the LearnLib implementation of the TTT algorithm for ALEX.
@@ -28,7 +29,56 @@ public class TTT implements LearnAlgorithmFactory {
         }
 
         TTTLearnerMealy tttLearner = (TTTLearnerMealy) learner;
-        return TTTSerializer.toJSON(tttLearner.getDiscriminationTree());
+        return toJSON(tttLearner.getDiscriminationTree());
+    }
+
+    /**
+     * Serializes the discrimination tree of the TTT algorithm into JSON.
+     *
+     * @param tree
+     *         The tree to convert into nice JSON.
+     * @return The JSON string of the given tree.
+     */
+    private String toJSON(de.learnlib.algorithms.ttt.base.DiscriminationTree<String, Word> tree) {
+        return toJSON(tree.getRoot());
+    }
+
+    private String toJSON(DTNode<String, Word> node) {
+        StringBuilder result = new StringBuilder();
+        result.append('{');
+
+        if (node.getParentEdgeLabel() != null) {
+            result.append("\"edgeLabel\": \"");
+            result.append(node.getParentEdgeLabel());
+            result.append("\",");
+        }
+
+        if (node.isLeaf()) {
+            result.append("\"data\": \"");
+            result.append(node.getState());
+            result.append('"');
+        } else {
+            result.append("\"discriminator\": \"");
+            result.append(node.getDiscriminator());
+            result.append("\", ");
+
+            result.append("\"children\": [");
+            node.getChildEntries().forEach(entry -> {
+                DTNode child = entry.getValue();
+                result.append(toJSON(child));
+                result.append(",");
+            });
+
+            // remove last ','
+            if (result.charAt(result.length() - 1) == ',') {
+                result.setLength(result.length() - 1);
+            }
+
+            result.append(']');
+        }
+
+        result.append('}');
+        return result.toString();
     }
 
 }
