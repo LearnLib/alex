c9bff7b08319c482351b8ced7a2d1fec224cae45
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2016-01-12 16:47:05 +0100

    added apache license header to js files

--- main/src/main/webapp/app/modules/controllers/modals/LearnResultDetailsModalController.js
+++ main/src/main/webapp/app/modules/controllers/modals/LearnResultDetailsModalController.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 TU Dortmund
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * The controller that is used to display the details of a learn result in a modal dialog. The data that is passed
  * to this controller should be an object with a property 'result' which contains a learn result object. If none is
