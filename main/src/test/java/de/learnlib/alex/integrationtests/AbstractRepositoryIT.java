9577bb1056e05ca235a0c128e613eb1e59bbb7ca
Alexander Bainczyk <alexander.bainczyk@outlook.de>
2017-08-22 11:39:08 +0200

    update backend dependencies

--- main/src/test/java/de/learnlib/alex/integrationtests/AbstractRepositoryIT.java
+++ main/src/test/java/de/learnlib/alex/integrationtests/AbstractRepositoryIT.java
@@ -21,11 +21,11 @@ import de.learnlib.alex.core.entities.Project;
 import de.learnlib.alex.core.entities.SymbolGroup;
 import de.learnlib.alex.core.entities.User;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = App.class)
+@ContextConfiguration(classes = App.class)
 public abstract class AbstractRepositoryIT {
 
     User createUser(String email) {
