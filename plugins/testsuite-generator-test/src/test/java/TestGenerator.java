8f78e106d81ecafc98369449a2caf255f6124cd6
Alexander Bainczyk <alexander.bainczyk@outlook.de>
2018-10-04 11:59:27 +0200

    generate test json file

---
+++ plugins/testsuite-generator-test/src/test/java/TestGenerator.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018 TU Dortmund
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.learnlib.alex.data.entities.ParameterizedSymbol;
+import de.learnlib.alex.data.entities.Symbol;
+import de.learnlib.alex.data.entities.SymbolActionStep;
+import de.learnlib.alex.data.entities.WebElementLocator;
+import de.learnlib.alex.data.entities.actions.web.ClearAction;
+import de.learnlib.alex.data.entities.actions.web.FillAction;
+import de.learnlib.alex.data.entities.actions.web.GotoAction;
+import de.learnlib.alex.data.entities.actions.web.PressKeyAction;
+import de.learnlib.alex.data.entities.actions.web.WaitForNodeAction;
+import de.learnlib.alex.testing.entities.TestCase;
+import de.learnlib.alex.testing.entities.TestCaseStep;
+import de.learnlib.alex.testing.entities.TestSuite;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestGenerator {
+
+    private static final long PROJECT_ID = 1L;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new IllegalAccessException("The argument for the path of the json file has to be specified.");
+        }
+
+        final String path = args[0];
+
+        final TestCaseStep s1 = new TestCaseStep();
+        s1.setPSymbol(createResetSymbol());
+
+        final TestCaseStep s2 = new TestCaseStep();
+        s2.setPSymbol(createSearchSymbol());
+
+        final TestCase testCase = new TestCase();
+        testCase.setName("TestGoogleSearch");
+        testCase.setProjectId(PROJECT_ID);
+        testCase.getSteps().add(s1);
+        testCase.getSteps().add(s2);
+
+        final TestSuite testSuite = new TestSuite();
+        testSuite.setName("GoogleTestSuite");
+        testSuite.setProjectId(PROJECT_ID);
+        testSuite.addTest(testCase);
+
+        final ObjectMapper om = new ObjectMapper();
+        final String ts = om.writeValueAsString(testSuite);
+
+        Files.write(Paths.get(path), ts.getBytes());
+    }
+
+    private static ParameterizedSymbol createResetSymbol() {
+        final GotoAction a1 = new GotoAction();
+        a1.setUrl("/");
+
+        final SymbolActionStep s1 = new SymbolActionStep();
+        s1.setAction(a1);
+
+        final WaitForNodeAction a2 = new WaitForNodeAction();
+        a2.setNode(new WebElementLocator("input[name='q']", WebElementLocator.Type.CSS));
+        a2.setWaitCriterion(WaitForNodeAction.WaitCriterion.CLICKABLE);
+        a2.setMaxWaitTime(5);
+
+        final SymbolActionStep s2 = new SymbolActionStep();
+        s2.setAction(a2);
+
+        final Symbol symbol = new Symbol();
+        symbol.setProjectId(PROJECT_ID);
+        symbol.setName("Reset");
+        symbol.getSteps().add(s1);
+        symbol.getSteps().add(s2);
+
+        final ParameterizedSymbol pSymbol = new ParameterizedSymbol();
+        pSymbol.setSymbol(symbol);
+
+        return pSymbol;
+    }
+
+    private static ParameterizedSymbol createSearchSymbol() {
+        final ClearAction a1 = new ClearAction();
+        a1.setNode(new WebElementLocator("input[name='q']", WebElementLocator.Type.CSS));
+
+        final SymbolActionStep s1 = new SymbolActionStep();
+        s1.setAction(a1);
+
+        final FillAction a2 = new FillAction();
+        a2.setNode(new WebElementLocator("input[name='q']", WebElementLocator.Type.CSS));
+        a2.setValue("potato");
+
+        final SymbolActionStep s2 = new SymbolActionStep();
+        s2.setAction(a2);
+
+        final PressKeyAction a3 = new PressKeyAction();
+        a3.setNode(new WebElementLocator("input[name='q']", WebElementLocator.Type.CSS));
+        a3.setKey("\\ue007");
+
+        final SymbolActionStep s3 = new SymbolActionStep();
+        s3.setAction(a3);
+
+        final Symbol symbol = new Symbol();
+        symbol.setProjectId(PROJECT_ID);
+        symbol.setName("Search");
+        symbol.getSteps().add(s1);
+        symbol.getSteps().add(s2);
+        symbol.getSteps().add(s3);
+
+        final ParameterizedSymbol pSymbol = new ParameterizedSymbol();
+        pSymbol.setSymbol(symbol);
+
+        return pSymbol;
+    }
+}
