8dbb9091da66b8ce5491253f7db7ed8e4e52d1c6
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2019-11-05 11:17:08 +0100

    redirect to index.html

---
+++ backend/src/main/java/de/learnlib/alex/RedirectConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 - 2019 TU Dortmund
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.learnlib.alex;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RedirectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry reg) {
+        redirect(reg, "/app/**", "/login", "/logout", "/error");
+    }
+
+    private void redirect(ViewControllerRegistry reg, String... routes) {
+        for (int i = 0; i < routes.length; i++) {
+            reg.addViewController(routes[i]).setViewName("forward:/index.html");
+        }
+    }
+}
